d1f2e2872a6473f473378c709b2ab03a4992a2bc
peng <1024371442@qq.com>
2016-12-29 09:14:14 +0800

    优化城市选择器，引入拖拽插件

--- app/js/project.js
+++ app/js/project.js
@@ -3,15 +3,75 @@ $(function () {
         history.go(-1)
     });
 
-    /* 封装一个手风琴插件 --start */
+    /* 手风琴 --start */
     $('[data-toggle="dropdown"]').on('click',function (e) {
         $(e.target).parent().toggleClass('show');
     });
     $('.icon-fangxiangshang').on('click',function (e) {
         $(e.target).parent().parent().toggleClass('show');
     });
-    /* 封装一个手风琴插件 --end */
+    /* 手风琴 --end */
+
+    /* 初始化客服按钮 */
+    $('#app').append('<div class="btn_kefu" id="btn_kefu"></div>').show();
+
+    setTimeout(function () {
+        initDrag('#btn_kefu');
+    },300);
 });
+
+/* 初始化拖拽插件 */
+function initDrag(id) {
+    delete Hammer.defaults.cssProps.userSelect;
+    var kefu = $(id)[0];
+    var hammer = new Hammer(kefu);
+
+    $(id).on('touchend', function () {
+        console.log('end');
+        $(id).css({
+            WebkitTransform: 'translate3d(0,0,0)scale(1)',
+            transform: 'translate3d(0,0,0)scale(1)'
+        });
+    }).on('touchstart', function (e) {
+        // 禁止浏览器默认选中事件
+        e.preventDefault();
+    });
+
+    hammer.on('press', function (e) {
+        console.log('press');
+        $(id).css({
+            WebkitTransform: 'translate3d(0,0,0) scale(1.1)',
+            transform: 'translate3d(0,0,0) scale(1.1)',
+            transition: 'all .3s ease-in-out',
+            opacity: 0.8
+        });
+    }).on('pan swipe', function (e) {
+        e.srcEvent.preventDefault();
+        /* 移动距离 */
+        var x = e.deltaX;
+        var y = e.deltaY;
+
+        $(id).css({
+            WebkitTransform: 'translate3d(' + x + 'px,' + y + 'px,' + 0 + ')scale(1.1)',
+            transform: 'translate3d(' + x + 'px,' + y + 'px,' + 0 + ')scale(1.1)',
+            transition: 'opacity .3s ease-in-out',
+        });
+        if (e.isFinal) {
+            var w = $(id).width()/2;
+            var h = $(id).height()/2;
+            var left = e.center.x;
+            var right = e.center.y;
+
+            $(id).css({
+                left:left - w,
+                top:right - h,
+                WebkitTransform: 'translate3d(0,0,0)scale(1)',
+                transform: 'translate3d(0,0,0)scale(1)',
+            });
+            console.log(e, '拖拽结束');
+        }
+    })
+}
 
 
 /* 封装toast弹框 */
